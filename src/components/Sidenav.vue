d1a066b5c118a1bcbc34828a3a672a8eda571dd7
Soumik Das <79033574+FalconEthics@users.noreply.github.com>
2023-01-08 22:09:32 +0530

    Update Sidenav.vue

--- src/components/Sidenav.vue
+++ src/components/Sidenav.vue
@@ -30,7 +30,7 @@
             <div v-if="dropdown === true"
                 class="absolute bottom-0 left-0 w-64 rounded-lg shadow-md border-lightest bg-white mb-16">
                 <button @click="dropdown = false"
-                    class="flex items-center w-full hover:bg-lightest p-2 focus:outline-none">
+                    class="flex items-center w-full hover:bg-lightcl p-2 focus:outline-none">
                     <img :src="`${'https://avatars.dicebear.com/api/personas/' + 100 + '.svg'}`" class="w-10 h-10 rounded-full border border-lighter" />
                     <div class="ml-4">
                         <p class="text-sm font-bold leading-tight"> Soumik Das </p>
@@ -39,11 +39,11 @@
                     <i class="fas fa-check ml-auto test-color"></i>
                 </button>
                 <button @click="dropdown = false"
-                    class="w-full text-left hover:bg-lightest border-t border-lighter p-3 test-sm focus:outline-none">
+                    class="w-full text-left hover:bg-lightcl border-t border-lighter p-3 test-sm focus:outline-none">
                     Add an existing account
                 </button>
                 <button @click="dropdown = false"
-                    class="w-full text-left hover:bg-lightest border-t border-lighter p-3 test-sm focus:outline-none">
+                    class="w-full text-left hover:bg-lightcl border-t border-lighter p-3 test-sm focus:outline-none">
                     Log out @SD783370
                 </button>
             </div>
